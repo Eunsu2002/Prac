677687c197a5725990665166915a2796fb1c43c4
unknown <zaml56@naver.com>
2024-06-04 00:31:46 +0900

    ✨ First commit
    first

---
+++ src/main/java/com/example/demo/dto/PostDTO.java
@@ -0,0 +1,29 @@
+package com.example.demo.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostDTO {
+    private int pid;
+
+    @NotEmpty
+    private String title;
+
+    @NotEmpty
+    @Size(min = 1, max = 100)
+    private String content;
+
+    @Builder.Default
+    private LocalDateTime regDate = LocalDateTime.now();
+    private LocalDateTime modDate;
+}
---
+++ src/main/java/com/example/demo/entity/Post.java
@@ -0,0 +1,42 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pid")
+    private int pid;
+
+    @Column(name = "nick_name")
+    private String nickName;
+
+    @Column(nullable = false, length = 600)
+    private String content;
+
+    @CreatedDate
+    @Builder.Default
+    @Column(name = "regDate", updatable = false)
+    private LocalDateTime regDate = LocalDateTime.now();
+
+    @LastModifiedDate
+    @Column(name = "modDate")
+    private LocalDateTime modDate;
+
+    public void changePost(String content){
+        this.content = content;
+    }
+}
